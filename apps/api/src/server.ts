8013b08c31fd1b7f8d066c3abc50814abf75f8f3
Daniel Ochieng <ochiengdaniel627@gmail.com>
2026-04-14 06:26:43 +0300

    init: turborepo monorepo scaffold with hono api

---
+++ apps/api/src/server.ts
@@ -0,0 +1,6 @@
+import { serve } from '@hono/node-server'
+import app from './index'
+
+serve({ fetch: app.fetch, port: 3001 }, () => {
+  console.log('Confessed API running on http://localhost:3001')
+})
